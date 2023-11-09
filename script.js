a32c483cd1179a3e5821cb8fea9efc8a9575b91f
erahasanaj <hasanajera0@gmail.com>
2023-11-09 16:52:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function edit() {
+    var element = document.getElementById('edit');
+    element.innerHTML = "Vanesa L";
+}
+
+function remove() {
+    var element = document.getElementById('remove');
+    element.remove(element);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
